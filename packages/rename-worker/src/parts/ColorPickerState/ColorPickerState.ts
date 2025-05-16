92fb10ab0ca3470a954c6756dbffcce27187085d
Le Vivilet <levivlet@pm.me>
2025-05-16 22:53:41 +0200

    remove unusd code

--- packages/rename-worker/src/parts/ColorPickerState/ColorPickerState.ts
+++
@@ -1,9 +0,0 @@
-import type { Rectangle } from '../Rectangle/Rectangle.ts'
-
-export interface ColorPickerState extends Rectangle {
-  readonly color: string
-  readonly offsetX: number
-  readonly max: number
-  readonly uid: number
-  readonly min: number
-}
