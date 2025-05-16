7ff81cd73eff0c889168c2c8f74dbf6f19701258
Le Vivilet <levivlet@pm.me>
2025-05-16 23:20:21 +0200

    fix

--- packages/rename-worker/src/parts/Clamp/Clamp.ts
+++ packages/rename-worker/src/parts/Clamp/Clamp.ts
@@ -1,6 +1,6 @@
 import * as Assert from '../Assert/Assert.ts'
 
-export const clamp = (num: number, min: number, max: number) => {
+export const clamp = (num: number, min: number, max: number): number => {
   Assert.number(num)
   Assert.number(min)
   Assert.number(max)
