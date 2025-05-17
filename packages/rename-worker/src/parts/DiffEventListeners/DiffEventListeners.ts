a1e1e35998f2b95c28e4c5f68ba2ec15371e4d7f
Le Vivilet <levivlet@pm.me>
2025-05-17 23:02:02 +0200

    fix

--- packages/rename-worker/src/parts/DiffEventListeners/DiffEventListeners.ts
+++
@@ -1,5 +0,0 @@
-import type { ColorPickerState } from '../ColorPickerState/ColorPickerState.ts'
-
-export const isEqual = (oldState: ColorPickerState, newState: ColorPickerState): boolean => {
-  return oldState.version === newState.version
-}
