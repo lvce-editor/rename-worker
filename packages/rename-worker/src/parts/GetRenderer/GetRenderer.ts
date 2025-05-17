e85b59c095137e2ac2209b30c9d78753164364fa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-18 00:01:45 +0200

    feature: render focus (#21)

--- packages/rename-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/rename-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -3,6 +3,7 @@ import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderBounds from '../RenderBounds/RenderBounds.ts'
 import * as RenderContent from '../RenderContent/RenderContent.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
+import * as RenderFocus from '../RenderFocus/RenderFocus.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
@@ -12,6 +13,8 @@ export const getRenderer = (diffType: number): Renderer => {
       return RenderBounds.renderBounds
     case DiffType.RenderEventListeners:
       return RenderEventListeners.renderEventListeners
+    case DiffType.RenderFocus:
+      return RenderFocus.renderFocus
     default:
       throw new Error('unknown renderer')
   }
