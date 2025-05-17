45cdd52486f9ba9693a8a9288a9ce82472b97407
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 23:14:47 +0200

    feature: register commands (#13)

--- packages/rename-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/rename-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,11 +1,17 @@
+import * as Accept from '../Accept/Accept.ts'
 import * as Create from '../Create/Create.ts'
+import * as Diff2 from '../Diff2/Diff2.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
+import * as HandleBlur from '../HandleBlur/HandleBlur.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
+  'Rename.accept': Accept.accept,
   'Rename.create': Create.create,
+  'Rename.diff2': Diff2.diff2,
   'Rename.getCommandIds': GetCommandIds.getCommandIds,
-  'Rename.terminate': Terminate.terminate,
+  'Rename.handleBlur': HandleBlur.handleBlur,
   'Rename.loadContent': LoadContent.loadContent,
+  'Rename.terminate': Terminate.terminate,
 }
