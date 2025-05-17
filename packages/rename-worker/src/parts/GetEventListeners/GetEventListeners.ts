f0cb7ddd7e7c44b68b768dfe954dac5c3462aa44
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 20:23:16 +0200

    feature: add event listeners (#5)

---
+++ packages/rename-worker/src/parts/GetEventListeners/GetEventListeners.ts
@@ -0,0 +1,5 @@
+import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
+
+export const getEventListeners = (uid: number, widgetId: number): readonly DomEventListener[] => {
+  return []
+}
