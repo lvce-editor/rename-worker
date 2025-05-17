20fe40ba828e420a7837c7ae3d4e1611829daa7e
Le Vivilet <levivlet@pm.me>
2025-05-17 20:19:55 +0200

    feature: add create function

---
+++ packages/rename-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -0,0 +1,15 @@
+import type { RenameState } from '../RenameState/RenameState.ts'
+
+export const createDefaultState = (): RenameState => {
+  return {
+    uid: 0,
+    height: 0,
+    focused: false,
+    focusedIndex: 0,
+    newValue: '',
+    oldValue: '',
+    width: 0,
+    x: 0,
+    y: 0,
+  }
+}
---
+++ packages/rename-worker/src/parts/LoadContent/LoadContent.ts
@@ -0,0 +1,7 @@
+import type { RenameState } from '../RenameState/RenameState.ts'
+
+export const loadContent = (state: RenameState): RenameState => {
+  return {
+    ...state,
+  }
+}
