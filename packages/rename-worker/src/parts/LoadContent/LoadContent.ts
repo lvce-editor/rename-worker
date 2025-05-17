fa7bf0ddd9dbdd7355e49b217ccc89c7165cf9e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 23:49:09 +0200

    feature: compute rename position in loadContent (#17)
    * feature: compute rename position in loadContent
    
    * tsc

--- packages/rename-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/rename-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,6 +1,17 @@
 import type { RenameState } from '../RenameState/RenameState.ts'
+import * as EditorCommandGetWordAt from '../EditorCommandGetWordAt/EditorCommandGetWordAt.ts'
+import * as GetPositionAtCursor from '../GetPositionAtCursor/GetPositionAtCursor.ts'
+import * as GetRenamePosition from '../GetRenamePosition/GetRenamePosition.ts'
 
-export const loadContent = (state: RenameState): RenameState => {
+export const loadContent = async (state: RenameState): Promise<Promise<RenameState>> => {
+  const { parentUid } = state
+  const { rowIndex, columnIndex, x, y } = await GetPositionAtCursor.getPositionAtCursor(parentUid)
+  const word = await EditorCommandGetWordAt.getWordAt(parentUid, rowIndex, columnIndex)
+  if (!word) {
+    return state
+  }
+  // TODO query if can rename from extension host
+  const { x: renameX, y: renameY, width, height } = GetRenamePosition.getRenamePosition(x, y)
   // TODO
   // 1. query position at cursor
   // 2. the bounds for position
@@ -8,5 +19,9 @@ export const loadContent = (state: RenameState): RenameState => {
   return {
     ...state,
     version: 1,
+    x: renameX,
+    y: renameY,
+    width,
+    height,
   }
 }
