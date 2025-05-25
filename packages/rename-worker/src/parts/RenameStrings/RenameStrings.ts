b17a52b50fb3b419ca71d0a37343701ccd983a2c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-25 11:45:44 +0200

    feature: add i18n strings (#27)

---
+++ packages/rename-worker/src/parts/RenameStrings/RenameStrings.ts
@@ -0,0 +1,6 @@
+import * as I18nString from '../I18NString/I18NString.ts'
+import * as UiStrings from '../UiStrings/UiStrings.ts'
+
+export const enterToRename = (): string => {
+  return I18nString.i18nString(UiStrings.EnterToRename)
+}
