5c1a37347c3f72b3f4de5f760f00ca7270836508
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-25 09:55:42 +0200

    feature: add rename event listeners (#24)

--- packages/rename-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/rename-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,8 +1,9 @@
 import * as DiffBounds from '../DiffBounds/DiffBounds.ts'
 import * as DiffContent from '../DiffContent/DiffContent.ts'
+import * as DiffEventListeners from '../DiffEventListeners/DiffEventListeners.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 
-export const modules = [DiffContent.isEqual, DiffBounds.isEqual, DiffFocus.isEqual]
+export const modules = [DiffContent.isEqual, DiffBounds.isEqual, DiffFocus.isEqual, DiffEventListeners.isEqual]
 
-export const numbers = [DiffType.RenderContent, DiffType.RenderBounds, DiffType.RenderFocus]
+export const numbers = [DiffType.RenderContent, DiffType.RenderBounds, DiffType.RenderFocus, DiffType.RenderEventListeners]
--- packages/rename-worker/src/parts/GetRenameVirtualDom/GetRenameVirtualDom.ts
+++ packages/rename-worker/src/parts/GetRenameVirtualDom/GetRenameVirtualDom.ts
@@ -1,20 +1,23 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { RenameState } from '../RenameState/RenameState.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getRenameVirtualDom = (state: RenameState): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
-      className: 'Viewlet EditorRename',
+      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.EditorRename),
       childCount: 1,
     },
     {
       type: VirtualDomElements.Input,
-      className: 'InputBox RenameInputBox',
+      className: MergeClassNames.mergeClassNames(ClassNames.InputBox, ClassNames.RenameInputBox),
       value: state.newValue,
       childCount: 0,
-      onBlur: 'handleBlur',
+      onBlur: DomEventListenerFunctions.HandleBlur,
     },
   ]
 }
