c5ca6e53b27d5e1c30ec063e91079cd544f59fbe
Le Vivilet <levivlet@pm.me>
2025-05-25 11:41:15 +0200

    lint

--- packages/rename-worker/src/parts/GetRenameVirtualDom/GetRenameVirtualDom.ts
+++ packages/rename-worker/src/parts/GetRenameVirtualDom/GetRenameVirtualDom.ts
@@ -1,8 +1,8 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { RenameState } from '../RenameState/RenameState.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetRenameInputVirtualDom from '../GetRenameInputVirtualDom/GetRenameInputVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
-import type { RenameState } from '../RenameState/RenameState.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getRenameVirtualDom = (state: RenameState): readonly VirtualDomNode[] => {
