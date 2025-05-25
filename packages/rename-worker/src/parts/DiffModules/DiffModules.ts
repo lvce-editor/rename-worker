f1b74c3231fa4f8a7e49cfe50a9b974e17536e26
Le Vivilet <levivlet@pm.me>
2025-05-25 11:54:31 +0200

    feature: render selection

--- packages/rename-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/rename-worker/src/parts/DiffModules/DiffModules.ts
@@ -2,9 +2,24 @@ import * as DiffBounds from '../DiffBounds/DiffBounds.ts'
 import * as DiffContent from '../DiffContent/DiffContent.ts'
 import * as DiffEventListeners from '../DiffEventListeners/DiffEventListeners.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
+import * as DiffSelection from '../DiffSelection/DiffSelection.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffContent.isEqual, DiffBounds.isEqual, DiffFocus.isEqual, DiffEventListeners.isEqual, DiffValue.isEqual]
+export const modules = [
+  DiffContent.isEqual,
+  DiffBounds.isEqual,
+  DiffFocus.isEqual,
+  DiffEventListeners.isEqual,
+  DiffValue.isEqual,
+  DiffSelection.isEqual,
+]
 
-export const numbers = [DiffType.RenderContent, DiffType.RenderBounds, DiffType.RenderFocus, DiffType.RenderEventListeners, DiffType.RenderValue]
+export const numbers = [
+  DiffType.RenderContent,
+  DiffType.RenderBounds,
+  DiffType.RenderFocus,
+  DiffType.RenderEventListeners,
+  DiffType.RenderValue,
+  DiffType.RenderSelection,
+]
--- packages/rename-worker/src/parts/DiffType/DiffType.ts
+++ packages/rename-worker/src/parts/DiffType/DiffType.ts
@@ -11,3 +11,4 @@ export const RenderCss = 10
 export const RenderEventListeners = 11
 export const RenderUid = 12
 export const RenderContent = 13
+export const RenderSelection = 14
