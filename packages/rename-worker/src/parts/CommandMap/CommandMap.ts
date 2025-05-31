dbd598b68d28be79ff2cf0dcace6292bc9ca6efa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 08:27:27 +0200

    feature: add dispose function (#49)
    * feature: add dispose function
    
    * fix

--- packages/rename-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/rename-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,7 @@
 import * as Accept from '../Accept/Accept.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
+import * as Dispose from '../Dispose/Dispose.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import * as HandleBlur from '../HandleBlur/HandleBlur.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
@@ -12,9 +13,10 @@ export const commandMap = {
   'Rename.accept': Accept.accept,
   'Rename.create': Create.create,
   'Rename.diff2': Diff2.diff2,
+  'Rename.dispose': Dispose.dispose,
   'Rename.getCommandIds': GetCommandIds.getCommandIds,
-  'Rename.handleBlur': HandleBlur.handleBlur,
+  'Rename.handleBlur': RenameStates.wrapCommand(HandleBlur.handleBlur),
   'Rename.loadContent': RenameStates.wrapCommand(LoadContent.loadContent),
-  'Rename.terminate': Terminate.terminate,
   'Rename.render2': Render2.render2,
+  'Rename.terminate': Terminate.terminate,
 }
---
+++ packages/rename-worker/src/parts/Dispose/Dispose.ts
@@ -0,0 +1,5 @@
+import * as RenameStates from '../RenameStates/RenameStates.ts'
+
+export const dispose = (uid: number): void => {
+  RenameStates.dispose(uid)
+}
