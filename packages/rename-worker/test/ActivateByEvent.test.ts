36cd2a25399e9c30f67d3ae30666fde0b6594ff7
Le Vivilet <levivlet@pm.me>
2025-06-24 20:11:03 +0200

    feature: add activate test

---
+++ packages/rename-worker/test/ActivateByEvent.test.ts
@@ -0,0 +1,16 @@
+import { test, expect, jest } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import { activateByEvent } from '../src/parts/ActivateByEvent/ActivateByEvent.ts'
+import * as EditorWorker from '../src/parts/EditorWorker/EditorWorker.ts'
+
+test('activateByEvent', async () => {
+  const mockInvokeFn = jest.fn()
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: mockInvokeFn,
+  })
+  EditorWorker.set(mockRpc)
+
+  await activateByEvent('event')
+  expect(mockInvokeFn).toHaveBeenCalledWith('ExtensionHostManagement.activateByEvent', 'event')
+})
