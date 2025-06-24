08967d7b4449967b0108abbde3ff5e3f6c5100a8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-24 21:09:49 +0200

    feature: add accept test (#95)
    * feature: add accept test
    
    * tsc
    
    * lint

--- packages/rename-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
+++ packages/rename-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
@@ -4,7 +4,9 @@ import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker
 export const execute = async ({ editor, args, event, method, noProviderFoundMessage, noProviderFoundResult = undefined }: any): Promise<any> => {
   const fullEvent = `${event}:${editor.languageId}`
   await ActivateByEvent.activateByEvent(fullEvent)
+  // TODO
+  const uid = 123
   // @ts-ignore
-  const result = await ExtensionHostWorker(method, editor.uid, ...args)
+  const result = await ExtensionHostWorker.invoke(method, uid, ...args)
   return result
 }
