86bf20c09f0a47d30f2cd4b7fd90cbd1ee54ba88
Le Vivilet <levivlet@pm.me>
2025-06-24 20:21:28 +0200

    tsc

--- packages/rename-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++ packages/rename-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
@@ -2,7 +2,6 @@ import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
 export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort2'
-  // @ts-ignore
   await EditorWorker.invokeAndTransfer(
     // @ts-ignore
     'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
