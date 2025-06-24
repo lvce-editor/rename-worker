fe492e6ba70dd450276609d1f4108bcd4b63727c
Le Vivilet <levivlet@pm.me>
2025-06-24 20:44:02 +0200

    feature: add renderer test

--- packages/rename-worker/test/GetRenderer.test.ts
+++ packages/rename-worker/test/GetRenderer.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@jest/globals'
 import * as DiffType from '../src/parts/DiffType/DiffType.js'
-import { getRenderer } from '../src/parts/GetRenderer/GetRenderer.js'
+import * as GetRenderer from '../src/parts/GetRenderer/GetRenderer.js'
 import * as RenderBounds from '../src/parts/RenderBounds/RenderBounds.js'
 import * as RenderContent from '../src/parts/RenderContent/RenderContent.js'
 import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.js'
@@ -9,34 +9,49 @@ import * as RenderFocusContext from '../src/parts/RenderFocusContext/RenderFocus
 import * as RenderSelection from '../src/parts/RenderSelection/RenderSelection.js'
 import * as RenderValue from '../src/parts/RenderValue/RenderValue.js'
 
-test('getRenderer returns correct renderer for RenderContent', () => {
-  expect(getRenderer(DiffType.RenderContent)).toBe(RenderContent.renderContent)
+test('getRenderer returns RenderContent.renderContent for RenderContent', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderContent)
+  expect(renderer).toBe(RenderContent.renderContent)
 })
 
-test('getRenderer returns correct renderer for RenderBounds', () => {
-  expect(getRenderer(DiffType.RenderBounds)).toBe(RenderBounds.renderBounds)
+test('getRenderer returns RenderBounds.renderBounds for RenderBounds', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderBounds)
+  expect(renderer).toBe(RenderBounds.renderBounds)
 })
 
-test('getRenderer returns correct renderer for RenderEventListeners', () => {
-  expect(getRenderer(DiffType.RenderEventListeners)).toBe(RenderEventListeners.renderEventListeners)
+test('getRenderer returns RenderEventListeners.renderEventListeners for RenderEventListeners', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderEventListeners)
+  expect(renderer).toBe(RenderEventListeners.renderEventListeners)
 })
 
-test('getRenderer returns correct renderer for RenderFocus', () => {
-  expect(getRenderer(DiffType.RenderFocus)).toBe(RenderFocus.renderFocus)
+test('getRenderer returns RenderFocus.renderFocus for RenderFocus', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderFocus)
+  expect(renderer).toBe(RenderFocus.renderFocus)
 })
 
-test('getRenderer returns correct renderer for RenderValue', () => {
-  expect(getRenderer(DiffType.RenderValue)).toBe(RenderValue.renderValue)
+test('getRenderer returns RenderValue.renderValue for RenderValue', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderValue)
+  expect(renderer).toBe(RenderValue.renderValue)
 })
 
-test('getRenderer returns correct renderer for RenderSelection', () => {
-  expect(getRenderer(DiffType.RenderSelection)).toBe(RenderSelection.renderSelection)
+test('getRenderer returns RenderSelection.renderSelection for RenderSelection', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderSelection)
+  expect(renderer).toBe(RenderSelection.renderSelection)
 })
 
-test('getRenderer returns correct renderer for RenderFocusContext', () => {
-  expect(getRenderer(DiffType.RenderFocusContext)).toBe(RenderFocusContext.renderFocusContext)
+test('getRenderer returns RenderFocusContext.renderFocusContext for RenderFocusContext', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderFocusContext)
+  expect(renderer).toBe(RenderFocusContext.renderFocusContext)
 })
 
-test('getRenderer throws error for unknown renderer', () => {
-  expect(() => getRenderer(999)).toThrow('unknown renderer')
+test('getRenderer throws error for unknown diff type', () => {
+  expect(() => GetRenderer.getRenderer(999)).toThrow('unknown renderer')
+})
+
+test('getRenderer throws error for negative diff type', () => {
+  expect(() => GetRenderer.getRenderer(-1)).toThrow('unknown renderer')
+})
+
+test('getRenderer throws error for zero diff type', () => {
+  expect(() => GetRenderer.getRenderer(0)).toThrow('unknown renderer')
 })
