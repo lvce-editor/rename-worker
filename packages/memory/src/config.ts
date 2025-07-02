ac17620e8ecaa6f0bc7747d43d9a722a1a2916f0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 19:12:52 +0200

    feature: reduce memory (#110)

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -7,6 +7,6 @@ export const workerPath = join(root, '.tmp/dist/dist/renameWorkerMain.js')
 
 export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
 
-export const instantiations = 5000
+export const instantiations = 2000
 
 export const instantiationsPath = join(root, 'packages', 'rename-worker')
