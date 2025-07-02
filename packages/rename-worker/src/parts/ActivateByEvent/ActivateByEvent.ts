ae841d8140c784b1599bd669df3bcca897e91210
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 19:08:50 +0200

    Discard changes to packages/rename-worker/src/parts/ActivateByEvent/ActivateByEvent.ts

--- packages/rename-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
+++ packages/rename-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
@@ -1,3 +1,6 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
-export const { activateByEvent } = EditorWorker
+export const activateByEvent = async (event: any): Promise<void> => {
+  // @ts-ignore
+  await EditorWorker.invoke('ExtensionHostManagement.activateByEvent', event)
+}
