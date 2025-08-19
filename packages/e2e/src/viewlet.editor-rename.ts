cfe656497461577677f1177861c75a2b005d1f58
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 14:35:22 +0200

    feature: add rename e2e tests (#127)

---
+++ packages/e2e/src/viewlet.editor-rename.ts
@@ -0,0 +1,28 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-rename'
+
+export const skip = true
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file.js`,
+    `let x = 1
+`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file.js`)
+  await Editor.setCursor(0, 5)
+
+  // act
+  await Editor.openRename()
+
+  // assert
+  const renameWidget = Locator('.EditorRename')
+  await expect(renameWidget).toBeVisible()
+  const renameInput = Locator('.RenameInputBox')
+  await expect(renameInput).toBeVisible()
+  await expect(renameInput).toBeFocused()
+}
