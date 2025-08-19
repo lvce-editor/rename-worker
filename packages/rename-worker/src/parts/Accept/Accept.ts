9429b59b8dfe7ab21bbc98271353cd3225450861
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 16:08:42 +0200

    feature: implement rename accept (#141)
    * feature: implement rename accept
    
    * lint

--- packages/rename-worker/src/parts/Accept/Accept.ts
+++ packages/rename-worker/src/parts/Accept/Accept.ts
@@ -1,21 +1,13 @@
 import type { RenameState } from '../RenameState/RenameState.ts'
+import * as ApplyWorkspaceEdit from '../ApplyWorkspaceEdit/ApplyWorkspaceEdit.ts'
+import { close } from '../Close/Close.ts'
+import * as ExtensionHostRename from '../ExtensionHostRename/ExtensionHostRename.ts'
+import * as GetOffsetAtCursor from '../GetOffsetAtCursor/GetOffsetAtCursor.ts'
 
 export const accept = async (state: RenameState): Promise<RenameState> => {
-  // TODO ask editor worker to apply changes
-  // const { widgets } = editor
-  // const newWidgets = RemoveEditorWidget.removeEditorWidget(widgets, WidgetId.Rename)
-  // TODO
-  // const offset = GetOffsetAtCursor.getOffsetAtCursor(editor)
-
-  // const result = await ExtensionHostRename.executeRenameProvider(editor, offset, child.newValue)
-  // @ts-ignore
-  // const changes = getRenameChanges(editor, result)
-  // 1. ask extension host for rename edits
-  // 2. apply rename edit across editor (and whole workspace)
-  // 3. close rename widget
-  return {
-    ...state,
-    focused: true,
-    // widgets: newWidgets,
-  }
+  const { parentUid, editorLanguageId, newValue } = state
+  const offset = await GetOffsetAtCursor.getOffsetAtCursor(parentUid)
+  const result = await ExtensionHostRename.executeRenameProvider(parentUid, editorLanguageId, offset, newValue)
+  await ApplyWorkspaceEdit.applyWorkspaceEdit(parentUid, result.edits)
+  return close(state)
 }
