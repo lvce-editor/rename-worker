b8cb2f79d700ab171ec9b4fe2ad4c9ddc4199f0c
Le Vivilet <levivlet@pm.me>
2025-08-19 16:38:05 +0200

    lint

--- packages/rename-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/rename-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,5 +1,5 @@
-import { getEventListeners } from '../GetEventListeners/GetEventListeners.ts'
 import type { RenameState } from '../RenameState/RenameState.ts'
+import { getEventListeners } from '../GetEventListeners/GetEventListeners.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 
 export const renderEventListeners = (state: RenameState): readonly any[] => {
