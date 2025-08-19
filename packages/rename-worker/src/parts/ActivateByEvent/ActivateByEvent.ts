888b92db20cbb00ab712b1a2f7c79a5ecd9581c6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 13:22:12 +0200

    feature: simplify activatebyEvent (#119)

--- packages/rename-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
+++ packages/rename-worker/src/parts/ActivateByEvent/ActivateByEvent.ts
@@ -1,6 +1,5 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
 export const activateByEvent = async (event: any): Promise<void> => {
-  // @ts-ignore
-  await EditorWorker.invoke('ExtensionHostManagement.activateByEvent', event)
+  await EditorWorker.activateByEvent(event)
 }
