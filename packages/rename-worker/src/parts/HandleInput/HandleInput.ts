18f11314b3ad9b8523db1156a3d0e7bbc27541ff
Le Vivilet <levivlet@pm.me>
2025-08-19 16:32:04 +0200

    feature: on input, set input source

--- packages/rename-worker/src/parts/HandleInput/HandleInput.ts
+++ packages/rename-worker/src/parts/HandleInput/HandleInput.ts
@@ -1,8 +1,10 @@
 import type { RenameState } from '../RenameState/RenameState.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
-export const handleInput = async (state: RenameState, value: string): Promise<RenameState> => {
+export const handleInput = async (state: RenameState, value: string, inputSource = InputSource.User): Promise<RenameState> => {
   return {
     ...state,
     newValue: value,
+    inputSource,
   }
 }
