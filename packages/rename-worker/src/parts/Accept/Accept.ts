970cb10f01c267ef74594d59c9de10cc2324c00f
Le Vivilet <levivlet@pm.me>
2025-08-19 16:07:44 +0200

    lint

--- packages/rename-worker/src/parts/Accept/Accept.ts
+++ packages/rename-worker/src/parts/Accept/Accept.ts
@@ -1,8 +1,8 @@
+import type { RenameState } from '../RenameState/RenameState.ts'
 import * as ApplyWorkspaceEdit from '../ApplyWorkspaceEdit/ApplyWorkspaceEdit.ts'
 import { close } from '../Close/Close.ts'
 import * as ExtensionHostRename from '../ExtensionHostRename/ExtensionHostRename.ts'
 import * as GetOffsetAtCursor from '../GetOffsetAtCursor/GetOffsetAtCursor.ts'
-import type { RenameState } from '../RenameState/RenameState.ts'
 
 export const accept = async (state: RenameState): Promise<RenameState> => {
   const { parentUid, editorLanguageId, newValue } = state
