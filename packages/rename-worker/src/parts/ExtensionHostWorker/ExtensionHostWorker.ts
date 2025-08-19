9ff03b067da782b58b9c5bea3a5c23ecb85a3b0f
Le Vivilet <levivlet@pm.me>
2025-08-19 15:24:05 +0200

    feature: simplify extension host rpc

--- packages/rename-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ packages/rename-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,11 +1,3 @@
 import { ExtensionHost } from '@lvce-editor/rpc-registry'
-import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
-// TODO use direct extension host connection
-export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
-  // @ts-ignore
-  const result = await EditorWorker.invoke('ExtensionHostWorker.invoke', method, ...params)
-  return result
-}
-
-export const { set } = ExtensionHost
+export const { invoke, set, dispose } = ExtensionHost
