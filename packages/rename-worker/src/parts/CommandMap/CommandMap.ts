82a057f0b45d86ffc9a073343023d11e21cd23f0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 13:48:25 +0200

    feature: add handleInput (#122)

--- packages/rename-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/rename-worker/src/parts/CommandMap/CommandMap.ts
@@ -6,6 +6,7 @@ import * as Diff2 from '../Diff2/Diff2.ts'
 import * as Dispose from '../Dispose/Dispose.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import * as HandleBlur from '../HandleBlur/HandleBlur.ts'
+import * as HandleInput from '../HandleInput/HandleInput.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as RenameStates from '../RenameStates/RenameStates.ts'
@@ -13,14 +14,15 @@ import * as Render2 from '../Render2/Render2.ts'
 
 export const commandMap = {
   'Rename.accept': Accept.accept,
+  'Rename.close': RenameStates.wrapCommand(Close.close),
   'Rename.create': Create.create,
   'Rename.diff2': Diff2.diff2,
   'Rename.dispose': Dispose.dispose,
   'Rename.getCommandIds': GetCommandIds.getCommandIds,
   'Rename.handleBlur': RenameStates.wrapCommand(HandleBlur.handleBlur),
+  'Rename.handleInput': HandleInput.handleInput,
+  'Rename.initialize': Initialize.initialize,
   'Rename.loadContent': RenameStates.wrapCommand(LoadContent.loadContent),
   'Rename.render2': Render2.render2,
-  'Rename.close': RenameStates.wrapCommand(Close.close),
   'Rename.terminate': terminate,
-  'Rename.initialize': Initialize.initialize,
 }
