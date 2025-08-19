0abe46a90d211483663234abe42f749c17558740
Le Vivilet <levivlet@pm.me>
2025-08-19 14:34:31 +0200

    feature: add rename e2e tests

---
+++ packages/e2e/src/viewlet.editor-rename-error.ts
@@ -0,0 +1,33 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-rename-error'
+
+export const skip = true
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file.js`,
+    `let x = 1
+`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file.js`)
+  await Editor.setCursor(0, 5)
+
+  await Editor.openRename()
+  const renameWidget = Locator('.EditorRename')
+  await expect(renameWidget).toBeVisible()
+  const renameInput = Locator('.RenameInputBox')
+  await expect(renameInput).toBeVisible()
+
+  // act
+
+  // TODO
+  // 1. type a value
+  // 2. press enter
+  // 3. verify an error message occurs
+
+  // assert
+}
