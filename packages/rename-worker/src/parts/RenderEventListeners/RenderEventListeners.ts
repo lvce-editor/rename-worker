c52a13f660187e917569a6e0c2c4c0f89749d116
Le Vivilet <levivlet@pm.me>
2025-08-19 16:36:50 +0200

    feature: simplify event listeners

--- packages/rename-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/rename-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,10 +1,9 @@
-import type { RenameState } from '../RenameState/RenameState.ts'
 import { getEventListeners } from '../GetEventListeners/GetEventListeners.ts'
+import type { RenameState } from '../RenameState/RenameState.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
-import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const renderEventListeners = (state: RenameState): readonly any[] => {
   const { uid } = state
-  const eventListeners = getEventListeners(uid, WidgetId.ColorPicker)
+  const eventListeners = getEventListeners()
   return [RenderMethod.SetEventListeners, uid, eventListeners]
 }
