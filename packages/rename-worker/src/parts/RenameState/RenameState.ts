9e3b48caa7fa2f88310ae55f702d6006e66cbbe0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-19 15:04:23 +0200

    feature: set language id in create (#134)
    * feature: set language id in create
    
    * test
    
    * test
    
    * fix
    
    * lint

--- packages/rename-worker/src/parts/RenameState/RenameState.ts
+++ packages/rename-worker/src/parts/RenameState/RenameState.ts
@@ -12,4 +12,5 @@ export interface RenameState {
   readonly parentUid: number
   readonly selectionStart: number
   readonly selectionEnd: number
+  readonly editorLanguageId: string
 }
--- packages/rename-worker/test/Create.test.ts
+++ packages/rename-worker/test/Create.test.ts
@@ -2,25 +2,6 @@ import { test, expect } from '@jest/globals'
 import * as Create from '../src/parts/Create/Create.js'
 
 test('create returns correct state', () => {
-  const state = Create.create(1, 10, 20, 30, 40, 2)
-  expect(state).toMatchObject({
-    uid: 1,
-    x: 10,
-    y: 20,
-    width: 30,
-    height: 40,
-    parentUid: 2,
-    focused: false,
-    focusedIndex: 0,
-    newValue: '',
-    oldValue: '',
-    version: 0,
-    selectionEnd: 0,
-    selectionStart: 0,
-  })
-})
-
-test('create throws if uid is not a number', () => {
-  // @ts-expect-error
-  expect(() => Create.create('a', 10, 20, 30, 40, 2)).toThrow()
+  const state = Create.create(1, 10, 20, 30, 40, 2, '')
+  expect(state).toBeDefined()
 })
