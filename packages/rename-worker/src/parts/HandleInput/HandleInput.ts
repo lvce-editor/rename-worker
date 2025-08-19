868691d3f132f300e8c6ca4b30f871e4292473ef
Le Vivilet <levivlet@pm.me>
2025-08-19 14:37:21 +0200

    feature: update value on input

--- packages/rename-worker/src/parts/HandleInput/HandleInput.ts
+++ packages/rename-worker/src/parts/HandleInput/HandleInput.ts
@@ -1,8 +1,8 @@
 import type { RenameState } from '../RenameState/RenameState.ts'
 
 export const handleInput = async (state: RenameState, value: string): Promise<RenameState> => {
-  // TODO set value
   return {
     ...state,
+    newValue: value,
   }
 }
