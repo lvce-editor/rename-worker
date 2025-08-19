5f48a5156c61d37876ac234fcfc0f9a4e997ba63
Le Vivilet <levivlet@pm.me>
2025-08-19 14:53:48 +0200

    fix

--- packages/rename-worker/test/Create.test.ts
+++ packages/rename-worker/test/Create.test.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@jest/globals'
 import * as Create from '../src/parts/Create/Create.js'
 
 test('create returns correct state', () => {
-  const state = Create.create(1, 10, 20, 30, 40, 2. '')
+  const state = Create.create(1, 10, 20, 30, 40, 2, '')
   expect(state).toBeDefined()
 })
