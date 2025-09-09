41d693f47d5409c7765e0e4c85bd34fd36c58767
Le Vivilet <levivilet@pm.me>
2025-09-09 23:22:02 +0200

    feature: remove unused code

--- packages/e2e/src/viewlet.editor-open-rename.ts
+++ packages/e2e/src/viewlet.editor-open-rename.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-rename'
 
-// export const skip = true
-
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
