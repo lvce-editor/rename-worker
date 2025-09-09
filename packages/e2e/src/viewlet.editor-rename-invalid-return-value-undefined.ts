a1a569734a43b764890a1c2b59a8890ac915b180
Le Vivilet <levivilet@pm.me>
2025-09-09 23:29:24 +0200

    feature: add invalid return value e2e test

---
+++ packages/e2e/src/viewlet.editor-rename-invalid-return-value-undefined.ts
@@ -0,0 +1,26 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-rename-invalid-return-value-undefined'
+
+export const skip = 1
+
+export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/sample.rename-provider')
+  await Extension.addWebExtension(extensionUri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file.xyz`,
+    `let x = 1
+`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file.xyz`)
+  await Editor.setCursor(0, 4)
+
+  // act
+  await Editor.rename2('y')
+
+  // assert
+  // TODO verify that error message is shown
+}
