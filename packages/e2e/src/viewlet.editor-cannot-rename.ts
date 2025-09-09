2f219b4c2d6397a6b8c45be7caa28a481ab5fa7e
Le Vivilet <levivilet@pm.me>
2025-09-09 23:23:15 +0200

    feature: add cannot rename e2e test

---
+++ packages/e2e/src/viewlet.editor-cannot-rename.ts
@@ -0,0 +1,30 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-cannot-rename'
+
+// export const skip = true
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file.js`,
+    `let x = 1
+`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file.js`)
+  await Editor.setCursor(0, 5)
+
+  // act
+  await Editor.openRename()
+
+  // assert
+  const renameWidget = Locator('.EditorRename')
+  await expect(renameWidget).toBeVisible()
+  await expect(renameWidget).toHaveCSS('top', '85px')
+  await expect(renameWidget).toHaveCSS('left', '45px')
+  const renameInput = Locator('.RenameInputBox')
+  await expect(renameInput).toBeVisible()
+  await expect(renameInput).toBeFocused()
+}
