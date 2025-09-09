0c2dad06d1b9d57b23a3fdf5f3ea67198690c3b8
Le Vivilet <levivilet@pm.me>
2025-09-09 18:55:21 +0200

    feature: hoist dom node (#168)

--- packages/rename-worker/src/parts/GetRenameDetailsVirtualDom/GetRenameDetailsVirtualDom.ts
+++ packages/rename-worker/src/parts/GetRenameDetailsVirtualDom/GetRenameDetailsVirtualDom.ts
@@ -3,14 +3,13 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as RenameStrings from '../RenameStrings/RenameStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
+const parentNode: VirtualDomNode = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.RenameDetails,
+  childCount: 1,
+}
+
 export const getRenameDetailsVirtualDom = (): readonly VirtualDomNode[] => {
   const detailsText = RenameStrings.enterToRename()
-  return [
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.RenameDetails,
-      childCount: 1,
-    },
-    text(detailsText),
-  ]
+  return [parentNode, text(detailsText)]
 }
