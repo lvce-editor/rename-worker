f1e20213016864385ce5dc5625ad23337168debe
Le Vivilet <levivilet@pm.me>
2025-09-12 10:39:50 +0200

    feature: add undo e2e test

---
+++ packages/e2e/src/viewlet.editor-rename-undo.ts
@@ -0,0 +1,28 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-rename-undo'
+
+export const skip = 1
+
+export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Editor, Command }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/sample.rename-provider')
+  await Extension.addWebExtension(extensionUri)
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file.xyz`,
+    `let x = 1
+`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file.xyz`)
+  await Editor.setCursor(0, 4)
+  await Editor.rename2('y')
+
+  // act
+  await Command.execute('Editor.undo')
+
+  // assert
+  await Editor.shouldHaveText(`let x = 1
+`)
+}
