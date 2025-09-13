81f555889b9ef40ee301ab25f7c06269c8676987
Le Vivilet <levivilet@pm.me>
2025-09-13 09:36:43 +0200

    feature: render either default dom or error dom depending on whether error message is set (#199)
    * feature: render either default dom or error dom depending on whether error message is set
    
    * lint

--- packages/rename-worker/src/parts/GetRenameVirtualDom/GetRenameVirtualDom.ts
+++ packages/rename-worker/src/parts/GetRenameVirtualDom/GetRenameVirtualDom.ts
@@ -1,19 +1,11 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { RenameState } from '../RenameState/RenameState.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as GetRenameDetailsVirtualDom from '../GetRenameDetailsVirtualDom/GetRenameDetailsVirtualDom.ts'
-import * as GetRenameInputVirtualDom from '../GetRenameInputVirtualDom/GetRenameInputVirtualDom.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { getRenameDefaultVirtualDom } from '../GetRenameDefaultVirtualDom/GetRenameDefaultVirtualDom.ts'
+import { getRenameErrorVirtualDom } from '../GetRenameErrorVirtualDom/GetRenameErrorVirtualDom.ts'
 
 export const getRenameVirtualDom = (state: RenameState): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.Div,
-      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.EditorRename),
-      childCount: 2,
-    },
-    ...GetRenameInputVirtualDom.getRenameInputVirtualDom(),
-    ...GetRenameDetailsVirtualDom.getRenameDetailsVirtualDom(),
-  ]
+  if (state.errorMessage) {
+    return getRenameErrorVirtualDom(state.errorMessage)
+  }
+  return getRenameDefaultVirtualDom(state)
 }
