6877c919c8e32770f5792dbd50cbeac84b5b7288
Le Vivilet <levivilet@pm.me>
2025-09-13 09:29:56 +0200

    feature: on accept error, set error message property (#198)

--- packages/rename-worker/src/parts/Accept/Accept.ts
+++ packages/rename-worker/src/parts/Accept/Accept.ts
@@ -3,11 +3,20 @@ import * as ApplyWorkspaceEdit from '../ApplyWorkspaceEdit/ApplyWorkspaceEdit.ts
 import { close } from '../Close/Close.ts'
 import * as ExtensionHostRename from '../ExtensionHostRename/ExtensionHostRename.ts'
 import * as GetOffsetAtCursor from '../GetOffsetAtCursor/GetOffsetAtCursor.ts'
+import * as Logger from '../Logger/Logger.ts'
 
 export const accept = async (state: RenameState): Promise<RenameState> => {
   const { parentUid, editorLanguageId, newValue } = state
   const offset = await GetOffsetAtCursor.getOffsetAtCursor(parentUid)
-  const result = await ExtensionHostRename.executeRenameProvider(parentUid, editorLanguageId, offset, newValue)
-  await ApplyWorkspaceEdit.applyWorkspaceEdit(parentUid, result.edits)
-  return close(state)
+  try {
+    const result = await ExtensionHostRename.executeRenameProvider(parentUid, editorLanguageId, offset, newValue)
+    await ApplyWorkspaceEdit.applyWorkspaceEdit(parentUid, result.edits)
+    return close(state)
+  } catch (error) {
+    await Logger.error(error)
+    return {
+      ...state,
+      errorMessage: `${error}`,
+    }
+  }
 }
