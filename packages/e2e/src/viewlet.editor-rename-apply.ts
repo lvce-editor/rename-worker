8badd57b47a4b1e5941ed94fceae29a86390b0d0
Le Vivilet <levivilet@pm.me>
2025-09-14 08:58:51 +0200

    feature: enable rename apply test

--- packages/e2e/src/viewlet.editor-rename-apply.ts
+++ packages/e2e/src/viewlet.editor-rename-apply.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-rename-apply'
 
-export const skip = 1
-
 export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Editor }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/sample.rename-provider')
