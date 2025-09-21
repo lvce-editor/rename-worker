e5fcc39d65b1bb8ba8bc03e2a6d7da3f08d13e61
Le Vivilet <levivilet@pm.me>
2025-09-21 11:54:39 +0200

    feature: improve apply error e2e test

--- packages/e2e/fixtures/sample.rename-provider-apply-error/main.js
+++ packages/e2e/fixtures/sample.rename-provider-apply-error/main.js
@@ -1,19 +1,12 @@
 const renameProvider = {
   languageId: 'xyz',
   provideRename(textDocument, offset, newName) {
-    // TODO compute rename
     return {
       canRename: true,
       edits: [
         {
           uri: textDocument.uri,
-          edits: [
-            {
-              offset: 4,
-              inserted: newName,
-              deleted: 1,
-            },
-          ],
+          edits: [null],
         },
       ],
     }
